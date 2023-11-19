89aec741f171afb63453ee2d5c9b1b4d4f792c5e
mihirh19 <miheerhadvani990@gmail.com>
2023-11-19 11:53:31 +0530

    setup

--- chatur_tolmap_server/settings.py
+++ chatur_tolmap_server/settings.py
@@ -79,7 +79,16 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER" : "postgres",
+#         "PASSWORD" : "chatur@34@tolmap",
+#         "HOST" : "db.ghethvsqjzvaateyzbcm.supabase.co",
+#         "PORT" : "5432",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
